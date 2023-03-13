e53b135087d2ee3cbe84e1ccfc5a0cb17d0f4256
songhyunseop <gustjq99455352@gmail.com>
2023-03-13 23:12:53 +0900

    1일차 송현섭 제출

---
+++ HyunseopSong/01/index.js
@@ -0,0 +1,25 @@
+
+
+import {Title} from "../../styles/emotion";
+
+import {header} from "../../styles/emotion";
+
+export default function EmotionPage() {
+    // 자바스크립트 작성공간3
+    
+    
+    
+    
+    
+        
+        //  html 작성공간 
+        return (
+    
+            <div>
+                <Title>
+                   <header></header>
+                </Title>
+              
+            </div>
+        );
+    };
