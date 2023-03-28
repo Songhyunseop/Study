bbfe5b7dbd1962795cbf54a9e7454c158a216dee
songhyunseop <gustjq99455352@gmail.com>
2023-03-28 17:38:11 +0900

    husky test

--- freeboard_frontend/pages/index.tsx
+++ freeboard_frontend/pages/index.tsx
@@ -73,7 +73,7 @@ export default function Home() {
 
           <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            // className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
