73b1df19de78e969bafb4626932e3fa26d04fa40
songhyunseop <gustjq99455352@gmail.com>
2023-08-31 17:25:44 +0900

    분기배포 작업

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "project-deployTest",
+
+  // 해당 주소들만 out 폴더에 생성해 줌
+  exportPathMap: () => {
+    return {
+      "/": { page: "/" },
+      "/board": { page: "/board" },
+      "/404": { page: "/404" },
+    };
+  },
 };
 
 module.exports = nextConfig;
